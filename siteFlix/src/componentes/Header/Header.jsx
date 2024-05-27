70dd1fa3c7ccea2e78de5f34e882105452fd6871
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 12:54:54 -0300

    portal de filmes

--- siteFlix/src/componentes/Header/Header.jsx
+++ siteFlix/src/componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "../Navbar/Navbar";
 
 function Header() {
     return ( 
-        <header className="flex justify-around bg-primary-raul text-zinc-100 p-5">
+        <header className="flex justify-around bg-black text-zinc-100 p-4">
             <span>Olá, visitante!</span>
             <h1>Bem-vindo ao MovieApp!</h1>
             <Navbar/>
--- siteFlix/src/pages/Home.jsx
+++ siteFlix/src/pages/Home.jsx
@@ -4,18 +4,28 @@ function Home() {
     console.log(data)
     
     return ( 
-        <main>
-            <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca"></input>
-            <div className='lista-filmes'>
-                {
-                    data.map(
-                        filme => (
-                            <h1>{filme.title}</h1>
-                        )
-                    )
-               }
-            </div>
-        </main>
+        <>
+        <input type="text" id="buscar" placeholder='Digite sua busca'/>
+        <div className='grid grid-cols-3 gap-4'> 
+        {data.map( filme => (
+                <div className='card' key={filme.title}>
+                    <h1>{filme.title}</h1>
+                    <img src={filme.image} alt={filme.title} />
+                    <div className='text'>
+                            {filme.text.map(text => (
+                                <span key={text}> {text} </span>
+                            ))}
+                    </div>
+                    <div className='tag'>
+                            {filme.tags.map(tag => (
+                                <span key={tag} className='text-purple-900'> {tag} </span>
+                            ))}
+                    </div><br></br>
+                </div>
+            ))
+        }
+        </div>
+        </>
         );
 }
 
